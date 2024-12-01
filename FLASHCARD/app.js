342d6aae998d9d5e9d8f6983f8c5080794da0e04
KauanAntonio300 <crislouquinho123@gmail.com>
2024-11-30 22:31:56 -0300

    Add files via upload

---
+++ FLASHCARD/app.js
@@ -0,0 +1,16 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+}
+
+container.appendChild(cartao)
+
+let respostaEstaVisivel = false
+
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+
+cartao.addEventListener('click', viraCartao)
